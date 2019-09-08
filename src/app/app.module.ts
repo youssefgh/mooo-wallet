d9c8b6570ae7b26a8d14bbe53affa52f956a63e0
Youssef GHOUBACH <ghoubach.youssef@gmail.com>
2019-09-08 18:03:21 +0000

    pwa support added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { AboutComponent } from './about/about.component';
 import { MnemonicDerivationComponent } from './mnemonic-derivation/mnemonic-derivation.component';
 import { ExtendedKeyDerivationComponent } from './extended-key-derivation/extended-key-derivation.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [
@@ -30,7 +32,8 @@ import { AppRoutingModule } from './app-routing.module';
         BrowserModule,
         FormsModule,
         HttpClientModule,
-        AppRoutingModule
+        AppRoutingModule,
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
     ],
     providers: [],
     bootstrap: [AppComponent]
